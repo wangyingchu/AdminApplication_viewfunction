6aab1deb30febb2397a0c465b3b84cb18c77d2c0
wangychu <yingchu-wang@163.com>
2016-03-03 21:17:43 +0800

    modify product name

--- src/com/MyVaadinApplication.java
+++ src/com/MyVaadinApplication.java
@@ -3,14 +3,11 @@ package com;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
 
-/**
- * Created by viewfunction on 2015/7/7.
- */
 public class MyVaadinApplication extends UI {
     @Override
     public void init(VaadinRequest request) {
         VerticalLayout layout = new VerticalLayout();
         setContent(layout);
-        layout.addComponent(new Label("Viewfunction Business Activity Management system"));
+        layout.addComponent(new Label("Viewfunction Business Activity Management system init page"));
     }
 }
